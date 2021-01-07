68850b060f37d0176800f81acbc85c2334fc396d
iumdgy <ong983179@gmail.com>
2021-01-08 01:44:36 +0900

    add socket client

---
+++ src/DiscordBot/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DiscordBot
+{
+    class Client
+    {
+        Socket client;
+
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Start();
+        }
+
+        public void Start()
+        {
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(new IPEndPoint(Dns.GetHostEntry("gateway.discord.gg").AddressList[0], 443));
+        }
+    }
+}
